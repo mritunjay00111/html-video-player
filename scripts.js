6bb614ec77e528bfbf8e464a0b0ea8b3cbab6c9d
Mritunjay Patel <mritunjay00111@gmail.com>
2026-01-07 10:11:43 +0530

    Add custom HTML5 video player
    Implemented a fully functional video player with custom controls and styling.

---
+++ scripts.js
@@ -0,0 +1,58 @@
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  toggle.textContent = video.paused ? "▶" : "⏸";
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRange() {
+  video[this.name] = this.value;
+}
+
+function handleProgress(){
+    const currDuration = (video.currentTime/video.duration) *100;
+    progressBar.style.flexBasis = `${currDuration}%`;
+}
+
+function handleScrub(e){
+ video.currentTime = ((e.offsetX/progress.offsetWidth)*video.duration);
+}
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+
+toggle.addEventListener("click", togglePlay);
+
+skipButtons.forEach((button) => {
+  button.addEventListener("click", skip);
+});
+
+ranges.forEach((range) => {
+  range.addEventListener("input", handleRange);
+});
+// let mousedown = false;
+progress.addEventListener('click',(e)=>{
+    handleScrub(e);
+});
+// progress.addEventListener('mousedown',(e)=>{ handleScrub(e);});
+// // progress.addEventListener('mousedown',()=>{mousedown = true;});
+// // progress.addEventListener('mouseup',()=>{mousedown = false});
